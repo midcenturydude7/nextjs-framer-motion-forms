c77843447197c8297231a7bb70a363a2fa07d2bd
midCenturyDude7 <mgriffes@gmail.com>
2024-03-05 16:44:16 -0800

    Created utils.js for clsx functionality

---
+++ app/components/Nav.tsx
@@ -0,0 +1,9 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import React from "react";
+
+export default function Nav() {
+  return <nav>Navbar</nav>;
+}
