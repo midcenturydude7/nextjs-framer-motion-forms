37206cf26a82f3c84d3118c970daf858e10c12fb
midCenturyDude7 <mgriffes@gmail.com>
2024-03-05 17:21:55 -0800

    Initial navbar setup

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,9 +1,53 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 import React from "react";
 
 export default function Nav() {
-  return <nav>Navbar</nav>;
+  const pathname = usePathname();
+
+  const links = [
+    {
+      path: "/",
+      name: "home",
+    },
+    {
+      path: "/inputs",
+      name: "inputs",
+    },
+    {
+      path: "/textareas",
+      name: "textareas",
+    },
+    {
+      path: "/radiobuttons",
+      name: "radio buttons",
+    },
+    {
+      path: "/checkboxes",
+      name: "checkboxes",
+    },
+    {
+      path: "/select",
+      name: "select",
+    },
+    {
+      path: "/specialty-inputs",
+      name: "specialty inputs",
+    },
+  ];
+
+  return (
+    <nav className="p-8">
+      <ul className="flex items-center justify-center gap-12">
+        {links.map((link) => (
+          <li key={link.path}>
+            <Link href={link.path}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
