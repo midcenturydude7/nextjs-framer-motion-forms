ec951ffb45d5f4ad376765a9fc583e6d6c96492a
midCenturyDude7 <mgriffes@gmail.com>
2024-03-08 20:19:39 -0800

    Updated styles and created textareas route

--- app/inputs/page.tsx
+++ app/inputs/page.tsx
@@ -2,7 +2,7 @@ import Inputs from "./components/Inputs";
 
 export default function page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+    <div className="flex min-h-screen items-start justify-center bg-slate-100 py-24">
       <Inputs />
     </div>
   );
---
+++ app/textareas/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Fun with textareas
+    </div>
+  );
+}
