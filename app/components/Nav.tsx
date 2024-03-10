343c9d69bf92419c1b98d87d210075c428286d6b
midCenturyDude7 <mgriffes@gmail.com>
2024-03-10 10:29:48 -0700

    Updated styles to indicate selected link via usePathname()

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -43,11 +43,18 @@ export default function Nav() {
     <nav className="p-8">
       <ul className="flex items-center justify-center gap-8">
         {links.map((link) => (
-          <li
-            key={link.path}
-            className="rounded-lg px-2 py-2 pl-4 pr-4 text-slate-600 transition-all duration-200 hover:bg-slate-700 hover:text-slate-300"
-          >
-            <Link href={link.path}>{link.name}</Link>
+          <li key={link.path}>
+            <Link
+              className={cn(
+                "rounded-lg px-2 py-2 pl-4 pr-4 text-slate-600 transition-all duration-200 hover:bg-slate-700 hover:text-slate-300",
+                pathname === link.path
+                  ? "cursor-default bg-slate-700 text-slate-300"
+                  : "",
+              )}
+              href={link.path}
+            >
+              {link.name}
+            </Link>
           </li>
         ))}
       </ul>
