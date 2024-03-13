64c19f67bbe16a591327db07e4ff3d70b910bab7
midCenturyDude7 <mgriffes@gmail.com>
2024-03-12 21:03:01 -0700

    Set up file/folder structure for specialty inputs

---
+++ app/specialty-inputs/components/SpecialtyInputs.tsx
@@ -0,0 +1,6 @@
+"use client";
+import React from "react";
+
+export default function SpecialtyInputs() {
+  return <div>Fun with Specialty Inputs</div>;
+}
---
+++ app/specialty-inputs/page.tsx
@@ -0,0 +1,9 @@
+import SpecialtyInputs from "./components/SpecialtyInputs";
+
+export default function page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100 py-24">
+      <SpecialtyInputs />
+    </div>
+  );
+}
